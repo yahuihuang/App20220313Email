4336366bd025d7e53c97543057847a0d73b7c85b
Huang Grace <huang.yahui.grace@gmail.com>
2022-03-13 15:43:29 +0800

    show login status

--- App20220313Email/ViewController.swift
+++ App20220313Email/ViewController.swift
@@ -10,14 +10,22 @@ import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var statusText: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        statusText.text = ""
         Auth.auth().addStateDidChangeListener { auth, user in
             if let user = user {
                 print("登入id\(user.uid)")
+                
+                self.statusText.text = self.statusText.text +
+                    "登入id: \(user.uid)\n" +
+                    "登入email: \(user.email ?? "")\n" +
+                    "檢查郵件狀態: \(user.isEmailVerified)\n"
             } else {
                 print("尚未登入")
+                self.statusText.text = self.statusText.text + "已登出\n"
             }
         }
     }
@@ -26,6 +34,7 @@ class ViewController: UIViewController {
         do {
             try Auth.auth().signOut()
             print("登出成功")
+            self.statusText.text = self.statusText.text + "登出成功\n"
         } catch {
             print(error.localizedDescription)
         }
